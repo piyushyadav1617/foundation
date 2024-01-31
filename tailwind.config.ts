db4cd232045e06ecf300a6b6ad5da4cd2d297cf0
piyush yadav <yadavpiyush222@gmail.com>
2024-01-31 21:49:43 +0530

    removes tailwind plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,7 +41,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"),require("tailwind-gradient-mask-image")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
