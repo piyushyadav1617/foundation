ad612859d69b5fe248e7867907f82ab836986c8e
piyush yadav <yadavpiyush222@gmail.com>
2024-02-09 23:43:07 +0530

    updated hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,30 +1,31 @@
 import Image from "next/image"
-import image from '@/public/daiga-ellaby-JZ51o_-UOY8-unsplash.jpg'
 import DonateBtn from "./donateBtn"
+// import DonateBtn from "./donateBtn"
 
 const Hero = ()=>{
     return (
-        <section>
+        <section className="bg-dot-black/[0.2] pb-6">
             <div className="pt-14 sm:pt-20 flex flex-col gap-10 md:flex-row justify-between mx-5 sm:mx-10 2xl:mx-auto 2xl:w-[1400px]">
                  <div className="flex flex-col gap-10 md:w-1/2">
                      
                      <p className="text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-semibold">The Power of <br/> Collective Giving</p>
                     
                       
-                      <p>
+                      <p className="bg-white">
                        Together, we can weave a tapestry of change, kindness, and enduring progress. Let&apos;s make a difference, one heart, one life at a time.
                       </p>
                       <DonateBtn/>
                  </div>
                  <div className="md:w-1/2 flex flex-col  gap-6 justify-center items-center md:items-end xl:items-center">
-                    <Image src={image} width={300} height={500} alt="Image" className="w-80 md:w-60 lg:w-80 shadow-lg shadow-black "/>
+                    <Image src={'/daiga-ellaby-JZ51o_-UOY8-unsplash.jpg'} width={300} height={500} alt="Image" className="w-80 md:w-60 lg:w-80 shadow-lg shadow-black "/>
                     
                     <div>
                     <hr className="h-2 bg-slate-400 w-40 sm:w-60 my-4 rounded-full"/>                  
-                    <p className="">
+                    <p className="bg-white">
                       Our mission is fueled by a commitment to uplift and empower, driven by the belief that every individual deserves a chance to thrive. At AliFoundation, we focus on key areas such as education, health, and environmental sustainability, striving to make a tangible difference in the lives we touch.
                       </p>
                       </div>
+                      
 
                  </div>
             </div>
